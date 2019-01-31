bcaff6afefdefbc49d58b969aed345c3e69912fe
Will Campbell <wtc@vt.edu>
2019-01-31 17:49:04 -0500

    Created empty file in aux-battery-adc branch

--- main.c
+++ main.c
@@ -66,44 +66,9 @@
 
 int main(void)
 {
-    // Enable the clock to GPIO Port N and wait for it to be ready
-    // The clock must be enabled to any GPIO port being used
-
-    uint32_t allSysPorts[GPIO_PORTS] = {SYSCTL_PERIPH_GPIOA, SYSCTL_PERIPH_GPIOB, SYSCTL_PERIPH_GPIOC, SYSCTL_PERIPH_GPIOD,
-                         SYSCTL_PERIPH_GPIOE, SYSCTL_PERIPH_GPIOF, SYSCTL_PERIPH_GPIOG, SYSCTL_PERIPH_GPIOH,
-                         SYSCTL_PERIPH_GPIOJ, SYSCTL_PERIPH_GPIOK, SYSCTL_PERIPH_GPIOL, SYSCTL_PERIPH_GPIOM,
-                         SYSCTL_PERIPH_GPION, SYSCTL_PERIPH_GPIOP, SYSCTL_PERIPH_GPIOQ};
-
-    uint32_t allGPIOPorts[GPIO_PORTS] = {GPIO_PORTA_BASE, GPIO_PORTB_BASE, GPIO_PORTC_BASE, GPIO_PORTD_BASE,
-                                         GPIO_PORTE_BASE, GPIO_PORTF_BASE, GPIO_PORTG_BASE, GPIO_PORTH_BASE,
-                                         GPIO_PORTJ_BASE, GPIO_PORTK_BASE, GPIO_PORTL_BASE, GPIO_PORTM_BASE,
-                                         GPIO_PORTN_BASE, GPIO_PORTP_BASE, GPIO_PORTQ_BASE};
-
-    int i;
-    for (i = 0; i < GPIO_PORTS; i++) {
-        MAP_SysCtlPeripheralEnable(allSysPorts[i]);
-        while(!(MAP_SysCtlPeripheralReady(allSysPorts[i]))) {};
-    }
-
-    uint32_t allPins = (GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);
-
-    // Configure pins being used (PN5 and PN4) for output
-    for (i = 0; i < GPIO_PORTS; i++) {
-        MAP_GPIOPinTypeGPIOOutput(allGPIOPorts[i], allPins);
-        MAP_GPIOPinWrite(allGPIOPorts[i], allPins, allPins);
-    }
-
-    // Write values to each pin
-    // param 1: the port
-    // param 2: the pins to be set
-    // param 3: the value (0 or 1), at bit-specific position
-    MAP_GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
 
     while (1)
     {
-        // a horrible way to switch LED on and off
-        int i;
-        for (i=0; i<1000000; i++) {}
-        MAP_GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, ~(MAP_GPIOPinRead(GPIO_PORTN_BASE, GPIO_PIN_0)));
+
     }
 }
