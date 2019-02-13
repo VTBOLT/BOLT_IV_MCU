804b052efda702514c33458ea92276562456a1f8
Will Campbell <wtc@vt.edu>
2019-02-13 07:45:18 -0500

    Implemented aux battery ADC and UART transmit to XBee

--- main.c
+++ main.c
@@ -32,7 +32,10 @@
 *
 ******************************************************************************
 *
-* MSP432 empty main.c template
+* MSP432 BOLT IV MCU PROGRAM
+*
+* Authors:
+* William Campbell
 *
 ******************************************************************************
 *
@@ -48,11 +51,10 @@
 *
 ******************************************************************************
 * Notes:
-* Must include "MAP_" before function calls, I don't know why, but otherwise
+* Must include "MAP_" before driverlib function calls, I don't know why, but otherwise
 *   it doesn't actually see the function and doesn't compile.
 *
 ******************************************************************************/
-
 
 #include "msp.h"
 
@@ -61,16 +63,122 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-#define GPIO_PORTS 15
+uint32_t systemClock;
+
+// Function prototypes
+void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count);
+void auxADCSetup();
+uint32_t auxADCSend(uint32_t* auxBatVoltage);
+void UART7Setup();
 
 int main(void)
 {
+    /* Configure system clock for 120 MHz */
+    systemClock = MAP_SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN |
+                                          SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480),
+                                          120000000);
+
+    uint32_t auxBatVoltage[1];
+    uint32_t auxBatAdjusted; //no decimal, accurate value
+    auxADCSetup();
+    UART7Setup();
 
     while (1)
     {
-        // test comment
-        // this updates aux-battery-ADC, not master
+        auxBatAdjusted = auxADCSend(auxBatVoltage);
 
+        MAP_SysCtlDelay(systemClock/2);
     }
+}
+
+void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
+{
+    //
+    // Loop while there are more characters to send.
+    //
+    while(ui32Count--)
+    {
+        //
+        // Write the next character to the UART.
+        //
+        MAP_UARTCharPutNonBlocking(UART7_BASE, *pui8Buffer++);
+    }
+}
+
+void auxADCSetup()
+{
+    /* AUX ADC SETUP - built using adc0_singleended_singlechannel_singleseq */
+
+    /* Enable the clock to GPIO Port E and wait for it to be ready */
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    while(!(MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))) {};
+
+    /* Configure PE0 as ADC input channel */
+    MAP_GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_0);
+
+    /* Enable the clock to ADC0 and wait for it to be ready */
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+    while(!(MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0))) {};
+
+    /* Configure Sequencer 3 to sample a single analog channel: AIN3 */
+    MAP_ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH3 | ADC_CTL_IE | ADC_CTL_END);
+
+    /* Configure and enable sample sequence 3 with a processor signal trigger */
+    MAP_ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+    MAP_ADCSequenceEnable(ADC0_BASE, 3);
+
+    /* Clear interrupt status flag */
+    MAP_ADCIntClear(ADC0_BASE, 3);
+}
+
+uint32_t auxADCSend(uint32_t* auxBatVoltage)
+{
+    /* AUX ADC */
+    MAP_ADCProcessorTrigger(ADC0_BASE, 3);
+    while(!MAP_ADCIntStatus(ADC0_BASE, 3, false)) {}
+    MAP_ADCIntClear(ADC0_BASE, 3);
+    MAP_ADCSequenceDataGet(ADC0_BASE, 3, auxBatVoltage);
+
+    uint8_t asciiChars[5];
+    float tempFloat = auxBatVoltage[0];
+
+    // From ((v/1000)/1.265)/.1904 - see spreadsheet
+    tempFloat *= 0.004152;
+    uint32_t temp = tempFloat * 100;
+    uint32_t toReturn = temp;
+
+    asciiChars[4] = temp % 10 + '0';
+    temp /= 10;
+    asciiChars[3] = temp % 10 + '0';
+    temp /= 10;
+    asciiChars[2] = '.';
+    asciiChars[1] = temp % 10 + '0';
+    temp /= 10;
+    asciiChars[0] = temp % 10 + '0';
+
+    UARTSend((uint8_t *)"AUX:", 4);
+    UARTSend(asciiChars, 5);
+    UARTSend((uint8_t *)"\n", 1);
+
+    return toReturn;
+}
+
+void UART7Setup()
+{
+    /* UART Transmit Setup */
+
+    /* Enable clock to peripherals used */
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART7);
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+
+    /* Set PC4 and PC5 as UART pins */
+    GPIOPinConfigure(GPIO_PC4_U7RX);
+    GPIOPinConfigure(GPIO_PC5_U7TX);
+    MAP_GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+
+    /* Configure UART for 57,600, 8-N-1 */
+    MAP_UARTConfigSetExpClk(UART7_BASE, systemClock, 57600,
+                            UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE);
 }
